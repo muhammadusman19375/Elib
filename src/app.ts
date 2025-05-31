954ccf945eb8b91acd28ad322e5f6baf99323e29
Musman <musman@goally.co>
2025-05-31 12:56:45 +0500

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import createHttpError from 'http-errors';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
 app.get('/', (req, res, next) => {
     res.json({message: "Welcome to ebook apis."});
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
