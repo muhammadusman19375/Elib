5dbe85dd827f1444bd9c55c31c33648dde573a9e
Musman <musman@goally.co>
2025-05-31 11:46:39 +0500

    add database setup

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import { start } from 'repl';
 import app from './src/app'
 import { config } from './src/config/config';
+import connectDB from './src/config/db';
 
 
-const startServer = () => {
+const startServer = async() => {
+    await connectDB();
+
     const port = config.port || 3000;
 
     app.listen(port, () => {
