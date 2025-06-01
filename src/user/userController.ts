475f81dd9e48262a0ebf2e541fab1f82ecffa884
Musman <musman@goally.co>
2025-06-01 17:24:56 +0500

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,8 @@ import userModel from "./userModel";
 import bcrypt from 'bcrypt';
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
+import { create } from "domain";
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
     const {name, email, password} = req.body;
@@ -14,27 +16,40 @@ const createUser = async(req: Request, res: Response, next: NextFunction) => {
     }
 
     // database call
-    const user = await userModel.findOne({email})
+    try {
+        const user = await userModel.findOne({email})
     if(user) {
         const error = createHttpError(400, "User already exists with this email.");
         return next(error);
+    }
+    } catch(err) {
+        return next(createHttpError(500, "Error while getting user."));
     }
 
     // password hash
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // logic process
-    const newUser = await userModel.create({
+    let newUser: User;
+    try {
+        newUser = await userModel.create({
         name,
         email,
         password: hashedPassword
     });
+    } catch(err) {
+        return next(createHttpError(500, "Error while creating user."))
+    }
 
-    // token generation: JWT
+    try {
+        // token generation: JWT
     const token = sign({sub: newUser._id}, config.jwtSecret as string, {expiresIn: '7d', algorithm: 'HS256'});
 
     // response
     res.json({accessToken: token});
+    } catch(err) {
+        return next(createHttpError(500, "Error while signing the jwt token."))
+    }
 };
 
 export {createUser};
