11a041d10571fe863795d1bcfc96e0cddbc89405
Musman <musman@goally.co>
2025-07-25 15:29:29 +0500

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express'
-import createHttpError from 'http-errors';
+import cors from 'cors';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
 import bookRouter from './book/bookRouter';
+import { config } from './config/config';
 
 const app = express();
+
+app.use(
+    cors({
+        origin: config.frontendDomain
+    })
+);
+
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
